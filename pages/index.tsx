f760f56862a8884184aac00667df14e4a89206d3
kcliu <gjliou@cs.nctu.edu.tw>
2022-01-12 07:14:19 +0900

    Add loading icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 import { SearchBar } from "../components/SearchBar/SearchBar";
 import Footer from "../components/Footer/Footer";
 import { RepoItem } from "../components/RepoItem/RepoItem";
+import { LoadingIcon } from "../components/LoadingIcon/LoadingIcon";
 type Item = {
   full_name: string;
   description: string;
@@ -18,15 +19,17 @@ const Home: NextPage = () => {
   const [query, setQuery] = useState("");
   const [items, setItems] = useState<Item[]>([]);
   const [pageInfo, setPageInfo] = useState({ current: FIRST_PAGE, total: 0 });
+  const [isLoading, setLoading] = useState<boolean>(false);
 
   const handleSearch = (page: number) => () => {
     if (!query) {
       return;
     }
     if (page === pageInfo.current) {
-        setPageInfo({current: FIRST_PAGE, total: 0})
-        page = FIRST_PAGE
+      setPageInfo({ current: FIRST_PAGE, total: 0 });
+      page = FIRST_PAGE;
     }
+    setLoading(true);
     fetch(
       `https://cors-anywhere.herokuapp.com/https://api.github.com/search/repositories?` +
         new URLSearchParams({
@@ -40,6 +43,7 @@ const Home: NextPage = () => {
     )
       .then((res) => res.json())
       .then((res) => {
+        setLoading(false);
         setItems(res.items);
         setPageInfo({ current: page, total: res.total_count });
       })
@@ -62,16 +66,22 @@ const Home: NextPage = () => {
           onClick={handleSearch(pageInfo.current)}
         />
         <div>
-          {items.map((item: Item, i) => {
-            return (
-              <RepoItem
-                key={i}
-                title={item.full_name}
-                description={item.description}
-                url={item.html_url}
-              />
-            );
-          })}
+          {isLoading ? (
+            <div className={styles.loadingContainer}>
+              <LoadingIcon />
+            </div>
+          ) : (
+            items.map((item: Item, i) => {
+              return (
+                <RepoItem
+                  key={i}
+                  title={item.full_name}
+                  description={item.description}
+                  url={item.html_url}
+                />
+              );
+            })
+          )}
         </div>
       </main>
       <Footer
