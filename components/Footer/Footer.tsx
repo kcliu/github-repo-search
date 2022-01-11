05fcdfcf1e0429fccc120ca00ca0432ca5caf19f
kcliu <gjliou@cs.nctu.edu.tw>
2022-01-12 06:10:13 +0900

    Update footer styles

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from "./Footer.module.css";
 const FIRST_PAGE = 1;
+const MAX_ITEMS = 1000;
 const Footer = ({
   page,
   perPage,
@@ -12,11 +13,32 @@ const Footer = ({
   handleSearch: (page: number) => () => void;
 }) => {
   const isFirstPage = page === FIRST_PAGE;
-  const isLastPage = page === Math.ceil(totalCount / perPage);
+  const totalItems = totalCount > MAX_ITEMS ? MAX_ITEMS : totalCount
+  const lastPage = Math.ceil(totalItems / perPage);
+  const isLastPage = page === lastPage;
+  console.log(page, lastPage)
   return (
     <footer className={styles.footer}>
-      {totalCount !== 0 && !isFirstPage && <button onClick={handleSearch(page - 1)}>PREV</button>}
-      {totalCount !== 0 && !isLastPage && <button onClick={handleSearch(page + 1)}>NEXT</button>}
+      {totalCount !== 0 && !isFirstPage && (
+        <button className={styles.button} onClick={handleSearch(FIRST_PAGE)}>
+          FIRST
+        </button>
+      )}
+      {totalCount !== 0 && !isFirstPage && (
+        <button className={styles.button} onClick={handleSearch(page - 1)}>
+          PREV
+        </button>
+      )}
+      {totalCount !== 0 && !isLastPage && (
+        <button className={styles.button} onClick={handleSearch(page + 1)}>
+          NEXT
+        </button>
+      )}
+      {totalCount !== 0 && !isLastPage && (
+        <button className={styles.button} onClick={handleSearch(lastPage)}>
+          LAST
+        </button>
+      )}
     </footer>
   );
 };
